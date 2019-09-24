ecd26be4c5dd1a23c6961c0539bda461fee12381
Petr Štetiar <ynezz@true.cz>
2019-09-24 23:37:42 +0200

    add basic logging
    In order to make further development easier.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- main.c
+++ main.c
@@ -4,12 +4,61 @@
  * SPDX-License-Identifier: LGPL-2.1
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include <libubox/uloop.h>
 
+#include "log.h"
 #include "ubus.h"
+
+#ifdef ULEDD_DEBUG
+unsigned int debug;
+#endif
+
+static int
+usage(const char *prog)
+{
+	fprintf(stderr, "Usage: %s [options]\n"
+		"Options:\n"
+#ifdef ULEDD_DEBUG
+		"	-d <level>	Enable debug messages\n"
+#endif
+		"	-S		Print messages to stdout\n"
+		"\n", prog);
+	return 1;
+}
 
 int main(int argc, char **argv)
 {
+	int ch;
+	int ulog_channels = ULOG_KMSG;
+#ifdef ULEDD_DEBUG
+	char *dbglvl = getenv("DBGLVL");
+
+	if (dbglvl) {
+		debug = atoi(dbglvl);
+		unsetenv("DBGLVL");
+	}
+#endif
+
+	while ((ch = getopt(argc, argv, "d:S")) != -1) {
+		switch (ch) {
+#ifdef ULEDD_DEBUG
+		case 'd':
+			debug = atoi(optarg);
+			break;
+#endif
+		case 'S':
+			ulog_channels = ULOG_STDIO;
+			break;
+		default:
+			return usage(argv[0]);
+		}
+	}
+
+	ulog_open(ulog_channels, LOG_DAEMON, "uledd");
 	uloop_init();
 	ubus_init();
 	uloop_run();
