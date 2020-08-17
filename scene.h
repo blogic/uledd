a6d44e583c78b3faa0db25f0586c1c982637a5ff
Joe Stefek <jstefek@datto.com>
2020-08-17 12:55:16 -0700

    ubus: Allow a scene to be stopped via ubus.
    Signed-off-by: Joe Stefek <jstefek@datto.com>

--- scene.h
+++ scene.h
@@ -10,6 +10,7 @@ struct scene* scene_add(const char *name, int timeout, int priority);
 void scene_led_add(struct scene *s, struct blob_led *b);
 const char* scene_str(struct scene *s);
 bool scene_run(const char *name);
+bool scene_stop(const char *name);
 void scene_done();
 
 void scenes_state_blobmsg(struct blob_buf *b);
